6192a3505f3d6b4b80d3d71fbbd64ac555cabc06
KingSiong <kingsiong@163.com>
2023-12-22 17:20:31 +0800

    first commit

---
+++ code.py
@@ -0,0 +1,8 @@
+import random
+
+def generate_random_number():
+    return random.randint(0, 9)
+
+# Call the function and print the result
+random_result = generate_random_number()
+print("Result:" random_result)
